6e8c3fc263142c9103ca574745428c63c14a8388
JohnnySy-dio <81820119+JohnnySy-dio@users.noreply.github.com>
2025-05-20 18:02:31 +0000

    Deploying to gh-pages from @ JohnnySy-dio/Lottery_Project@77b0f5475a2c7b1b8c3f9007658fd43eed0a213e 🚀

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
         // Sepolia testnet
         "0xaa36a7": "0xa9684d2C0Da71d1F3107cf9d45CcD57e5a528C30",
         // Ganache local
-        "0x539": "0x672ced8E1B1fF58b34406410e221BC6220167cFf",  // Will be updated after local deployment
+        "0x539": "0x557bC432D9D2BcbE16CeB1202C9Bd8236410A424",  // Will be updated after local deployment
         // Add more networks as needed
     },
     
